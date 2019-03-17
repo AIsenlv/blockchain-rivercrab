5cd7b709a079d0c5a049e6f2c679abde4b95d1e2
lvjianmin <1415368564@qq.com>
2019-03-17 22:40:01 +0800

    heheh

--- src/components/basicmessage/birthCategory.vue
+++ src/components/basicmessage/birthCategory.vue
@@ -9,7 +9,36 @@
       <div class="div_left">
         <div class="div_tree">
           <div class="el-tree">
-            <ggg @showbox="toshow" :msg="dateArr"></ggg>
+            <!-- <ggg @showbox="toshow" :msg="dateArr"></ggg> -->
+             <div class="el-tree-node" v-for="a in dateArr" :key="a.id">
+                        <div class="el-tree-node__content" :class="a.txt?'acisnt':''" @click.stop.self="colorclick(a)" >
+                            <span class="el-tree-node__expand-icon" :class="a.valid || a.next.length=='0'?'expanded':''" :style="'margin-left:'+(a.inx==1?'100':(100+a.inx*15))+'px'"
+                              @click.stop.self="colorclick(a)"
+                            ></span>
+                            <span class="el-tree-node__label" @click.stop.self="colorclick(a)">{{a.text}}</span>
+                        </div>
+                        <div class="el-tree-node__children" v-if="a.valid">
+                                 <div class="el-tree-node" v-for="b in a.next" :key="b.id">
+                                    <div class="el-tree-node__content" :class="b.txt?'acisnt':''" @click.stop.self="colorclick(b)" >
+                                        <span class="el-tree-node__expand-icon" :class="b.valid || b.next.length=='0'?'expanded':''" :style="'margin-left:'+(b.inx==1?'100':(100+b.inx*15))+'px'"
+                                          @click.stop.self="colorclick(b)"
+                                        ></span>
+                                        <span class="el-tree-node__label" @click.stop.self="colorclick(b)">{{b.text}}</span>
+                                    </div>
+                                    <div class="el-tree-node__children" v-if="b.valid">
+                                             <div class="el-tree-node" v-for="c in b.next" :key="c.id">
+                                                <div class="el-tree-node__content" :class="c.txt?'acisnt':''" @click.stop.self="colorclick(c)" >
+                                                    <span class="el-tree-node__expand-icon" :class="c.valid || c.next.length=='0'?'expanded':''" :style="'margin-left:'+(c.inx==1?'100':(100+c.inx*15))+'px'"
+                                                      @click.stop.self="colorclick(c)"
+                                                    ></span>
+                                                    <span class="el-tree-node__label" @click.stop.self="colorclick(c)">{{c.text}}</span>
+                                                </div>
+                                                
+                                            </div>
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
             <!---->
           </div>
         </div>
@@ -57,19 +86,19 @@ export default {
       dateArr: [
         {
           text: "水产品",
-          valid: false,
+          valid: true,
           inx: 1,
           txt: false,
           next: [
             {
               text: "蟹类",
-              valid: false,
+              valid: true,
               inx: 2,
               next: [
-                { text: "稻田蟹", valid: false, txt: false, inx: 3, next: [] },
-                { text: "湖里蟹", valid: false, txt: false, inx: 3, next: [] },
-                { text: "河蟹", valid: false, txt: false, inx: 3, next: [] },
-                { text: "红蟹", valid: false, txt: false, inx: 3, next: [] }
+                { text: "稻田蟹", valid: true, txt: false, inx: 3, next: [] },
+                { text: "湖里蟹", valid: true, txt: false, inx: 3, next: [] },
+                { text: "河蟹", valid: true, txt: false, inx: 3, next: [] },
+                { text: "红蟹", valid: true, txt: false, inx: 3, next: [] }
               ]
             }
           ]
@@ -80,12 +109,47 @@ export default {
   },
   methods: {
     toshow(arr) {
-      this.date = [
-        { name: "产前", type: "启用" },
-        { name: "产中", type: "启用" },
-        { name: "产后", type: "启用" }
-      ];
-    }
+      if(arr.text=="河蟹" || arr.text=="红蟹"){
+          this.date = [
+            { name: "产前", type: "启用" },
+            { name: "产中", type: "启用" },
+            { name: "产后", type: "启用" }
+          ];
+      }else{
+          this.date = [
+            { name: "产前2", type: "启用" },
+            { name: "产中2", type: "启用" },
+            { name: "产后2", type: "启用" }
+          ];
+      }
+      
+    },
+     colorclick(a) {
+          this.dateArr.forEach(item => {
+            if (a.inx == item.inx && a.text == item.text) {
+              item.valid = !item.valid;
+            } else{
+               item.next &&  item.next.forEach(resf=>{
+                    if (a.inx == resf.inx && a.text == resf.text) {
+                    resf.valid = !resf.valid;
+                  }else{
+                    resf.next && resf.next.forEach(onts=>{
+                        if (a.inx == onts.inx && a.text == onts.text) {
+                            onts.valid = !onts.valid;
+                            onts.txt = true;
+                            this.toshow(onts);
+                        }else{
+                            onts.txt = false;
+                        }
+                    })
+                      
+                  }  
+                })
+                  
+            
+            }
+          });
+        }
   },
   components: {
     ggg: {
@@ -107,27 +171,7 @@ export default {
               `,
       props: ["msg"],
       methods: {
-        colorclick(a) {
-           this.$emit("showbox");
-          this.msg.forEach(item => {
-            if (a.inx == item.inx && a.text == item.text) {
-              item.valid = !item.valid;
-              if (
-                a.inx == item.inx &&
-                a.text == item.text &&
-                item.next.length == 0
-              ) {
-                this.$emit("showbox");
-                item.txt = true;
-              }
-            } else if (item.next.length > 0) {
-              find(item.next, a);
-            } else {
-              item.txt = false;
-              return;
-            }
-          });
-        }
+       
       },
       data() {
         return {};
